22775c74b4218af669155d48f4218affb47327fa
Sundarapandi T <131521909+SundarapandiT@users.noreply.github.com>
2024-03-31 16:10:50 +0530

    Create Confirmation1.java

---
+++ Src/Confirmation1.java
@@ -0,0 +1,14 @@
+package com.example.rrtvs;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.activity.ComponentActivity;
+
+public class Confirmation1 extends ComponentActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.servicemain2);
+    }
+}
